cbbd1b8cf445c891e3c50acc86512e5e7930ec81
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-09-15 14:33:26 +0530

    Create inline_function.cpp

---
+++ inline_function.cpp
@@ -0,0 +1,21 @@
+//# Object-Oriented-Programming
+//Here I can publish my Object Oriented Programs
+
+//Inline function implementation
+
+#include<iostream>
+using namespace std;
+inline int m(int x,int y){
+   return (x>y)?x:y;
+     }
+    
+ int main (){
+
+     cout<<"Max is: "<<m(10,20)<<endl;
+     cout<<"Max is: "<<m(15,5)<<endl;
+      return 0;
+    
+    }
+
+
+
