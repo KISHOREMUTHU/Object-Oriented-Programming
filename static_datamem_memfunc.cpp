08abff8b8a972beeffa1f8f3658d34ceea39a84d
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-09-15 11:20:00 +0530

    Create static_datamem_memfunc.cpp

---
+++ static_datamem_memfunc.cpp
@@ -0,0 +1,36 @@
+//# Object-Oriented-Programming
+//Here I can publish my Object Oriented Programs
+
+//Static data member and member function 
+//implementation
+
+#include<iostream>
+using namespace std;
+
+
+class data{
+
+
+     static int a;  //static data member
+     public :
+
+    // static member function
+  static int pre_increment(){
+   cout<<"Value after function called: "<<++a<<endl;
+      
+  }
+        };
+
+ int data::a=10; 
+
+int main()
+{
+ 
+  class data a;
+
+ a.pre_increment();
+ a.pre_increment();
+ 
+ return 0;
+      
+}
