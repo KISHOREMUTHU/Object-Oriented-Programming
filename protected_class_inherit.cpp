7f2a964a72e6cb62b49377f09aaa8f26f8b2c39a
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-09-17 15:21:24 +0530

    Create protected_class_inherit.cpp

---
+++ protected_class_inherit.cpp
@@ -0,0 +1,55 @@
+//# Object-Oriented-Programming
+//Here I can publish my Object Oriented Programs
+
+//Single inheritance with protected access specifier implementation
+
+#include<iostream>
+using namespace std;
+
+class base{
+      
+
+        int a;
+        
+      public:  
+
+        //Getting data from user
+      int get(int temp){
+       
+           a=temp;
+          return a;
+          }
+        
+        void print(){
+           cout<<"Data in base class: "<<a<<endl;
+              }
+            
+        };
+        
+class derived : protected base{
+
+         public:
+        
+          int b;
+        
+        //Function inside derived class 
+         void show (){
+            print();//This will print either garbage value or zero (compiler dependent)
+            b=get(15)+10;//This line will derive the get() function from base class
+            cout<<"Data in derived class: "<<b<<endl;
+            print(); // This print() function will print 10 because ,the base class function is derived in the previous statement 
+             }
+            
+            };
+            
+int main (){
+
+      class derived d;// declaring object of derived class 
+       d.show(); //d.get() and d.print() doesn't work directly because derived class is protected
+  //d.show() contains printing conditions of both base class and derived class
+    
+        return 0;
+        
+ 
+        
+    }
