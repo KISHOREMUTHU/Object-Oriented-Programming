ccacab577ab038740eed82c3507baf4b82f214b2
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-09-17 15:13:05 +0530

    Update friend_class.cpp

--- friend_class.cpp
+++ friend_class.cpp
@@ -6,30 +6,50 @@
 #include<iostream>
 using namespace std;
 
-class B;
+class B; //friend class 
 
 class A{
+
       int val;
+
     public:
+
     A(){
-     cin>>val; }
-    friend int sum(A,B);
+     cin>>val; 
+      }
+
+    friend int sum(A,B); //friend function for addition
+
      };
+
 class B{
+
       int val;
+
      public:
+
     B(){
           cin>>val;
        }
-    friend int sum(A,B);
+
+    friend int sum(A,B);//same friend function
+
     };
+
  int sum(A v1, B v2){
-      cout<<v1.val+v2.val;}
+
+      cout<<v1.val+v2.val; // Adding values of two different classes in friend function
+  
+}
+
   int main(){
+
      A a;
      B b;
      sum(a,b);
-     return 0; }  
+     return 0; 
+
+}  
     
     
    
