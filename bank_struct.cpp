987e589cd218eba30673e209a7f7d7ddd6f2cad8
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-09-17 15:06:37 +0530

    Update bank_struct.cpp

--- bank_struct.cpp
+++ bank_struct.cpp
@@ -1,7 +1,7 @@
 //# Object-Oriented-Programming
 //Here I can publish my Object Oriented Programs
 
-// Bank system using struct
+// Bank system using structures
 
 #include<iostream>
 #include<string.h>
@@ -10,12 +10,16 @@ using namespace std;
 struct bank{
    int acno;
    char name[25];
-   double amount;};
+   double amount;
+    };  //structure declaration
 
 int main(){
-  struct bank b[5];
+  struct bank b[5]; //array of structures
+
   int i;
+
  cout<<"Reading details:"<<endl<<endl;
+
  for(i=0;i<5;i++){
      cout<<"Person - "<<i+1<<endl;
     cout<<"Enter account number: ";
@@ -23,14 +27,18 @@ int main(){
     cout<<"Enter person's name: ";
      cin>>b[i].name;
     cout<<"Enter present amount: ";
-     cin>>b[i].amount;}
+     cin>>b[i].amount;
+         }
     cout<<endl<<endl;
  
 cout<<"Displaying Details"<<endl;
+
  for(i=0;i<5;i++)
 {
       cout<<"Person - "<<i+1<<endl;
       cout<<"Account number: "<<b[i].acno<<endl;
       cout<<"Person's name: "<<b[i].name<<endl;
-       cout<<"Present Amount: "<<b[i].amount<<endl<<endl;}
+       cout<<"Present Amount: "<<b[i].amount<<endl<<endl;
+}
+
     }
