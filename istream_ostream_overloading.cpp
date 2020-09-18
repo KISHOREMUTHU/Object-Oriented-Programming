a0c85cde0ecedd494572f2f011887b9c62dc02df
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-09-18 09:59:00 +0530

    Update istream_ostream_overloading.cpp

--- istream_ostream_overloading.cpp
+++ istream_ostream_overloading.cpp
@@ -8,23 +8,42 @@
 using namespace std;
  
 class test {
+
      int val;
+
      public:
+
     test(){
-    val=0;
-    }
+     val=0; // Default constructor
+          } 
+
     friend ostream & operator << (ostream &,test &);
     friend istream & operator >> (istream &,test &);
+   
     };
-  ostream & operator <<(ostream &kout,test &temp){
+
+   //ostream is the keyword for overloading "<<" operator
+
+  ostream & operator <<(ostream &kout,test &temp)
+           {
          kout<<temp.val;
-        return kout;}
-  istream & operator >>(istream &kin, test &temp){
+        return kout;
+           }
+
+ // istream is the keyword for overloading ">>" operator 
+
+  istream & operator >>(istream &kin, test &temp)
+          {
           kin>>temp.val;
-         return kin;}
+         return kin;
+          }
         
     int main(){
-       test t;
-         cin>>t;
-         cout<<t;
-        return 0;}
+
+         test t;
+         cin>>t; //Overloading ">>" operator
+         cout<<t; //Overloading "<<" operator
+
+        return 0;
+
+       }
