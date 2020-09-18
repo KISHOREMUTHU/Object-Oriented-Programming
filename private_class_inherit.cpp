6b8bf05d6775282a745c28468c37de60b18e9dc2
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-09-18 10:25:20 +0530

    Update private_class_inherit.cpp

--- private_class_inherit.cpp
+++ private_class_inherit.cpp
@@ -12,26 +12,31 @@ class base{
         int a;
         
         //Getting data from user 
-      int get(){
+      int get()
+          {
          cout<<"Enter data: ";
            a=10;
           return a;
           }
         
-        void print(){
+       //Printing data
+        void print()
+             {
            cout<<"Data in base class: "<<a<<endl;
               }
             
         };
         
 class derived : private base{
+
          public:
         
           int b;
         
          //Function inside derived class 
 
-         void show (){
+         void show ()
+               {
             print();
             b=a+10;
             cout<<"Data in derived class: "<<b<<endl;
@@ -42,7 +47,7 @@ class derived : private base{
 int main (){
 
       class derived d; // declaring object of derived class 
-       d.show();.  //d.get() and d.print() doesn't work directly because derived class is private
+       d.show(); //d.get() and d.print() doesn't work directly because derived class is private
     //d.show() contains printing conditions of both base class and derived class
     
         return 0;
